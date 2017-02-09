87ced3aa6fcd3cafc1e8a3739adfa641300cca72
Sabiner <chaoyue_ge@163.com>
2017-02-09 11:42:01 +0800

    Moved Fts into their own individual files

---
+++ superlists/functional_tests/test_layout_and_styling.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+from .base import FunctionalTest
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        # 伊迪丝访问首页
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 她会看到输入框完美的居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        ## 通过一定数学计算，判断输入框是否位于正中，指定结果在正负五像素范围内都可以接受
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        import time
+        time.sleep(1)
+
+        # 她新建了一个清单，看到输入框仍完美的居中显示
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
---
+++ superlists/functional_tests/test_list_item_validation.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+from unittest import skip
+from .base import FunctionalTest
+
+class ItemValidationTest(FunctionalTest):
+
+    @skip
+    def test_cannot_and_empty_list_items(self):
+        pass
