6508383c7dfd8866e620c3b093d11a06685f9416
Sabiner <chaoyue_ge@163.com>
2017-02-10 11:28:28 +0800

    Refactor hard-coded URLs out of templates

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -2,6 +2,7 @@
 
 from unittest import skip
 from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
 
@@ -9,7 +10,7 @@ class ItemValidationTest(FunctionalTest):
         # 伊迪丝访问首页，不小心提交了一个空待办事项
         # 输入框中没输入内容，她就按下了回车键
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # 首页刷新了，显示一个错误信息
         # 提示待办事项不能为空
@@ -17,11 +18,16 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 她输入一些文字，然后再次提交，这次没问题了
-        self.browser.find_element_by_id('id-new_item').send_keys('Buy milk\n')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy milk')
 
         # 她有点儿调皮，又提交了一个空待办事项
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        import time
+        time.sleep(2)
 
         # 在列表页面中她看到了一个类似的错误信息
         self.check_for_row_in_list_table('1: Buy milk')
@@ -29,6 +35,12 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 输入文字之后就没问题了
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Make tea')
+        input_box.send_keys(Keys.ENTER)
+
+        import time
+        time.sleep(2)
+
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
