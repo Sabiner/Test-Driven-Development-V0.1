e63847c07eac4fb1b630af959d4abbe4b9926663
Sabiner <chaoyue_ge@163.com>
2017-02-15 11:36:04 +0800

    Implement duplicate item validation at model layout

--- superlists/lists/admin.py
+++ superlists/lists/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+# -*- coding:utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from .models import List, Item
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('text', 'list')
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Item, ItemAdmin)
